ebe38ed59a4ef0a0ddf9151b855bd0a478fd431f
Marcos Felipe <p.marcosfelipe@gmail.com>
2020-10-12 17:04:30 -0300

    create orphanages map structure

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './screens/Landing';
-import Orphanages from './screens/Orphanages';
+import OrphanagesMap from './screens/OrphanagesMap';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Landing}></Route>
-        <Route path="/app" component={Orphanages}></Route>
+        <Route path="/app" component={OrphanagesMap}></Route>
       </Switch>
     </BrowserRouter>
   );
--- web/src/screens/Landing.tsx
+++ web/src/screens/Landing.tsx
@@ -4,7 +4,6 @@ import { FiArrowRight } from 'react-icons/fi';
 
 import logoImg from '../images/logo.svg';
 
-import '../styles/global.css';
 import '../styles/pages/landing.css';
 
 function Landing() {
